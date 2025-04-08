52309bb6da01ccf957cbb7aa81c90bf3519b575f
nahom zenebe <nahomzenebe123@gmail.com>
2025-04-08 19:51:51 +0300

    creating an delete for teacher

--- server/controller/Teachercontroller.js
+++ server/controller/Teachercontroller.js
@@ -78,7 +78,7 @@ module.exports.createTeacherprofile=async(req,res)=>{
 }
 
 
-module.exports.createTeacherprofile=async(req,res)=>{
+module.exports.getTeacher=async(req,res)=>{
     try{
         const teachers=await Teacher.find()
         .populate('subjects')
@@ -136,6 +136,23 @@ module.exports.updateTeacher=async(req,res)=>{
     catch(error){
         res.status(400).json({ error: error.message });
 
+    }
+}
+
+
+module.exports.deleteTeacher=async(req,res)=>{
+    try{
+        const {TeacherId}=req.params;
+        const deletedTeacher=await Teacher.findByIdAndDelete(TeacherId)
+            if (!deletedTeacher) {
+                return res.status(404).json({ error: 'Teacher not found' });
+              }
+        res.status(200).json({ message: 'Teacher deleted successfully' });
+
+    
+    }
+    catch(error){
+        res.status(500).json({ error: error.message });
     }
 }
 
--- server/router/Teacherrouter.js
+++ server/router/Teacherrouter.js
@@ -1,10 +1,13 @@
 const express=require("express")
 const router=express.Router()
-const {createTeacherprofile}=require('../controller/TeacherController')
+const {createTeacherprofile,getTeacher,getTeacherById,updateTeacher,deleteTeacher}=require('../controller/TeacherController')
 
 
 router.post("/TeacherProfile",createTeacherprofile)
-
+router.get('/getallTeacher', getTeacher);
+router.get('/getTeacher/:TeacherId', getTeacherById);
+router.put('/updateTeacher/:TeacherId', updateTeacher);
+router.delete('/deleteTeacher/:TeacherId', deleteTeacher);
 
 
 
