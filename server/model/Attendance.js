0ce4ceb482f9724e54c282d7998424068b6a245f
nahom zenebe <nahomzenebe123@gmail.com>
2025-04-17 23:53:19 +0300

    addingadmin model

--- server/model/Attendance.js
+++ server/model/Attendance.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const AttendanceSchema = new Schema({
+const AttendanceSchema = new mongoose.Schema({
  
   studentId: {
     type: mongoose.Schema.Types.ObjectId,
