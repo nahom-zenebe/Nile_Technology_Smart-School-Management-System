8b35b7f4bda5e92e64a20adce5fc9d7453f6cd24
nahom zenebe <nahomzenebe123@gmail.com>
2025-04-19 13:39:58 +0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './index.css'; 
+function App() {
+  return (
+    <div className="App">
+<h1 className="bg-green-500 text-white text-2xl p-4 rounded">
+  Hello Tailwind v3
+</h1>
+
+    </div>
+  );
+}
+
+export default App;
